f961458805901b244872a25ec5fec868f77f6ead
Damir <dkolobaric@gmail.com>
2018-11-21 15:13:26 +0100

    es6

--- src/slider.js
+++ src/slider.js
@@ -4,14 +4,14 @@ import gray_right from './images/arrow-gray-right.png';
 import gray_left from './images/arrow-gray-left.png';
 import blue_right from './images/arrow-blue-right.png';
 import blue_left from './images/arrow-blue-left.png';
-$(document).ready(function () {
+$(document).ready(() => {
 
     /** Change color of left arroww image on hover */
-    $('.left').hover(function () {
+    $('.left').hover(() => {
 
         $("#left_img").attr("src", gray_left);
 
-    }, function () {
+    }, () => {
 
         $("#left_img").attr("src", blue_left);
 
@@ -19,11 +19,11 @@ $(document).ready(function () {
 
 
     /** Change color of right arroww image on hover */
-    $('.right').hover(function () {
+    $('.right').hover(() => {
 
         $("#right_img").attr("src", gray_right);
 
-    }, function () {
+    }, () => {
 
         $("#right_img").attr("src", blue_right);
 
@@ -32,7 +32,7 @@ $(document).ready(function () {
     /** Devide li elements */
     $("li").each(function (index) {
 
-        var li_length = $("#slider li").length;
+        let li_length = $("#slider li").length;
 
         if (index < li_length / 2) {
 
@@ -46,7 +46,7 @@ $(document).ready(function () {
 
     });
 
-    var slider = $('#slider');
+    let slider = $('#slider');
 
     /** Wrap all images */
     slider.find('.slider-row-one-image').wrapAll('<div class="slider-row-one" />');
@@ -54,15 +54,15 @@ $(document).ready(function () {
 
 
 
-    var slider_row_one = $('#slider').find('.slider-row-one');
-    var slider_row_two = $('#slider').find('.slider-row-two');
+    let slider_row_one = $('#slider').find('.slider-row-one'),
+        slider_row_two = $('#slider').find('.slider-row-two');
 
     /** Slide to left */
-    slider.find('.left').click(function () {
+    slider.find('.left').click(() => {
 
         slider_row_one.animate({
             'left': -slider_row_one.find('li:last').width()
-        }, 100, function () {
+        }, 100, () => {
 
             slider_row_one.append(slider_row_one.find('li:first')).css('left', '0');
 
@@ -70,7 +70,7 @@ $(document).ready(function () {
 
         slider_row_two.animate({
             'left': -slider_row_two.find('li:last').width()
-        }, 100, function () {
+        }, 100, () => {
 
             slider_row_two.append(slider_row_two.find('li:first')).css('left', '0');
 
@@ -79,11 +79,11 @@ $(document).ready(function () {
     });
 
     /** Slide to right */
-    slider.find('.right').click(function () {
+    slider.find('.right').click(() => {
 
         slider_row_one.animate({
             'left': slider_row_one.find('li:last').width()
-        }, 100, function () {
+        }, 100, () => {
 
             slider_row_one.prepend(slider_row_one.find('li:last')).css('left', '0');
 
@@ -91,7 +91,7 @@ $(document).ready(function () {
 
         slider_row_two.animate({
             'left': slider_row_two.find('li:last').width()
-        }, 100, function () {
+        }, 100, () => {
 
             slider_row_two.prepend(slider_row_two.find('li:last')).css('left', '0');
 
